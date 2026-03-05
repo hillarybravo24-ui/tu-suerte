0b3ae252ab110dfbf904a8687e0635087f52bdaa
Hillary Bravo <hillarybravo24@gmail.com>
2026-03-05 18:32:50 -0500

    primer version

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express")
+
+const app = express()
+
+app.use(express.static("public"))
+
+app.listen(3000, function(){
+console.log("Servidor funcionando en http://localhost:3000")
+})
